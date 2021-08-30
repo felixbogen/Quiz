ab3f6451ebccd96d9afed4386b18eff1624fa354
Felix <felixb@askeris.no>
2021-08-30 21:31:25 +0200

    DB

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+let server;
+
+//middleware config
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: "20mb" }));
+app.use(bodyParser.json({ limit: "20mb" }));
+
+mongoose
+  .connect(process.env.DB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Database Connection Established"))
+  .catch((er) => console.log("Error Connection to mongoose instance", er));
+
+server = app.listen(PORT, () => {
+  console.log(`node server listening on port ${PORT}`);
+});
